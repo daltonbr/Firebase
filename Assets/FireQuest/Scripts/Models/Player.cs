5faac9f354f7c8f8c055385b71f1e43dbf00aa4c
daltonbr <daltonvarussa@gmail.com>
2018-10-11 20:42:08 -0300

    add Player

---
+++ Assets/FireQuest/Scripts/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿public class Player
+{
+    #region Properties
+
+    public string email;
+    public int score;
+    public int level;
+
+    #endregion
+
+    public Player(string email, int score, int level)
+    {
+        this.email = email;
+        this.score = score;
+        this.level = level;
+    }
+
+}
